e46823986c44ed3fea4a432bbf5308bf40add02a
Charlie <charlie@getwrecked.com>
2016-07-22 18:59:18 -0700

    Deploy script
    Simplify the process of deploying the blog

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Ensure the public folder is gone
+rm -rf public
+
+# Build the project.
+hugo
+
+# Move to the public dir
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+# git subtree push --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
